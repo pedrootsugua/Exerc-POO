6e8eef884eb0f1c929ae0b61bf478f181c3c4a49
pedrootsugua <pedro.zanele@gmail.com>
2024-03-09 23:38:33 -0300

    Add construtores e metodos acessores

--- src/main/java/com/mycompany/exerc7_poo/Consulta.java
+++ src/main/java/com/mycompany/exerc7_poo/Consulta.java
@@ -9,12 +9,57 @@ import java.time.LocalTime;
  */
 public class Consulta {
 
+    private int id;
+    private static int count = 1;
     private LocalDate dataConsulta;
     private LocalTime horarioConsulta;
     private String tipoConsulta;
     
+    //constructors
+    public Consulta() {
+        this.id = count++;
+    }
+
+    public Consulta(LocalDate dataConsulta, LocalTime horarioConsulta, String tipoConsulta) {
+        this.dataConsulta = dataConsulta;
+        this.horarioConsulta = horarioConsulta;
+        this.tipoConsulta = tipoConsulta;
+        this.id = count++;
+    }
+    
+     // getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public LocalDate getDataConsulta() {
+        return dataConsulta;
+    }
+
+    public void setDataConsulta(LocalDate dataConsulta) {
+        this.dataConsulta = dataConsulta;
+    }
+
+    public LocalTime getHorarioConsulta() {
+        return horarioConsulta;
+    }
+
+    public void setHorarioConsulta(LocalTime horarioConsulta) {
+        this.horarioConsulta = horarioConsulta;
+    }
+
+    public String getTipoConsulta() {
+        return tipoConsulta;
+    }
+
+    public void setTipoConsulta(String tipoConsulta) {
+        this.tipoConsulta = tipoConsulta;
+    }
+    
     //criar metodo que cria uma consulta, recebe o paciente como parametro e vincula a consulta a ele
-    
-    
 
 }
--- src/main/java/com/mycompany/exerc7_poo/Paciente.java
+++ src/main/java/com/mycompany/exerc7_poo/Paciente.java
@@ -7,11 +7,63 @@ package com.mycompany.exerc7_poo;
 public class Paciente {
 
     private int id;
+    private static int count = 1;
     private String nome;
     private int idade;
-    private Consulta consultas[];
-    private static int count = 1;
+    private Consulta[] consultas = new Consulta[1];
+    private int countConsulta = 0;
+
+    //constructors
+    public Paciente() {
+        this.id = count++;
+    }
+
+    public Paciente(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.id = count++;
+    }
     
-    //criar metodo de agendar consulta que insere uma consulta no vetor Consultas
+    // getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Consulta[] getConsultas() {
+        return consultas;
+    }
+
+    public void setConsultas(Consulta[] consultas) {
+        this.consultas = consultas;
+    }
+    
+     //criar metodo de agendar consulta que insere uma consulta no vetor Consultas
+    public void agendarConsulta(Consulta consulta) {
+        if(countConsulta >= consultas.length) {
+            
+        }
+        consultas[countConsulta++] = consulta;
+    }
+    
 
 }
